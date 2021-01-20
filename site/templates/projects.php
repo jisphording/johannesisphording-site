d271b0b537620fdcc6346265ab054a6cfb5066a9
Johannes Isphording <mail@johannesisphording.de>
2021-01-20 17:17:05 +0100

    [php][template] project overview

---
+++ site/templates/projects.php
@@ -0,0 +1,22 @@
+<?php snippet('header') ?>
+
+	<main class="main" role="main">
+
+		<!-- LOCAL PAGE HEADER -->
+		<header>
+			<!-- Intro -->
+			<div class="intro text <?= $page ?>">
+				<?= $page->text()->kirbytext() ?>
+			</div>
+		</header>
+		
+		<div class="wide">
+		
+		<!-- Showcase Loop -->
+		<?php $site->displayShowcase('projects', 99) ?>
+
+		</div>
+
+	</main>
+
+<?php snippet('footer') ?>
