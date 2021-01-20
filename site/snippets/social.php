6992afb578cd6114b16362cccc79b239d88bd001
Johannes Isphording <mail@johannesisphording.de>
2021-01-20 17:48:32 +0100

    [php] adding a small social media menu snippet

---
+++ site/snippets/social.php
@@ -0,0 +1,25 @@
+<!-- SOCIAL MEDIA CHANNELS MENU -->
+<nav class="meta social menu" role="navigation">
+
+	<ul class="social-options">
+		<li class="menu-item">
+			<a href="https://www.behance.net/isphording">Behance</a>
+		</li>
+		<li>
+			<a href="https://dribbble.com/isphording">Dribbble</a>
+		</li>
+		<li>
+			<a href="https://www.github.com/jisphording">GitHub</a>
+		</li>
+		<li>
+			<a href="https://www.instagram.com/studioisphording/">Instagram</a>
+		</li>
+		<li>
+			<a href="https://medium.com/@johannesisphording">Medium</a>
+		</li>
+		<li>
+			<a href="https://vimeo.com/jisphording">Vimeo</a>
+		</li>
+	</ul>
+
+</nav>
