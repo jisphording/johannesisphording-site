26f2665bb486f4c6e7f708e0a4b6fe0beaad295f
Johannes Isphording <mail@johannesisphording.de>
2021-01-20 18:16:52 +0100

    [php][snip] Added Office Location

---
+++ site/snippets/visitenkarte-bln.php
@@ -0,0 +1,21 @@
+<!-- VISITENKARTE / OFFICE LOCATION DETAILS -->
+<section class="visitenkarte">
+	<div class="visitenkarte-overlay">
+		<ul>
+			<li><em>Johannes Isphording</em><br />
+			Digital Designer &amp; Writing about it</li>
+
+			<li><strong>Berlin</strong></li>
+
+			<li><em>New Studio</em><br />
+			c/o Johannes Isphording<br />
+			Prinzessinnenstr. 16<br />
+			10969 Berlin</li>
+
+			<li>mail@johannesisphording.de<br />
+			+49 176 26 19 7704</li>
+		</ul>
+	</div>
+
+	<img src="<?= $page->url() . '/' . $page->visitenkarte() ?>">
+</section>
