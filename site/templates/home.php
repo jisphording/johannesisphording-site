98604584cb93d4a3e71e1f527723c77804f9bb29
Johannes Isphording <mail@johannesisphording.de>
2021-01-20 13:27:09 +0100

    [php][template] Init Homepage template

---
+++ site/templates/home.php
@@ -0,0 +1,56 @@
+<?php snippet('header') ?>
+
+  <main class="main landing" role="main">
+    <!-- LOCAL PAGE HEADER -->
+    <header>
+		<!
+      	<section class="showreel">
+			<!-- Showreel -->
+			<video playsinline autoplay muted loop poster="<?= $site->uri() . '/content/home/' . $page->showreel() ?>.jpg">
+				<source src="<?= $site->uri() . '/content/home/' . $page->showreel() ?>.webm" type="video/webm" />
+				<source src="<?= $site->uri() . '/content/home/' . $page->showreel() ?>.ogg" type="video/ogg" />
+				Sorry, your browser doesn't support embedded videos, but don't worry, you can <a href="<?= $site->url() . '/content/home/' . $page->showreel() ?>.mp4">download it</a>
+				and watch it with your favorite video player!
+			</video>
+
+			<!-- Showreel Headline -->
+			<h1><?= $page->title() ?></h1>
+      	</section>
+    </header>
+  
+	<!-- PROJECT SHOWCASE -->
+    <section class="projects-section">
+
+		<!-- Showcase Loop -->
+		<?php $site->displayShowcase('projects', 4) ?>
+
+		<!-- Link to all projects -->
+		<p class="projects-section-more">
+			<a href="<?= page('projects')->url() ?>" class="btn"><?php echo t('Show all projects') ?></a>
+		</p>
+      
+    </section>
+	
+	<!-- ARTICLE SHOWCASE -->
+    <section class="articles-section">	
+      
+		<h3><?php echo t('Recent articles') ?></h3>
+		
+		<!-- Section Description -->
+        <div class="intro text">
+          <?= $page->intro()->kirbytext() ?>
+        </div>
+
+        <!-- Sub Pages Loop -->
+        <?php $site->pullRelatedPages('articles') ?>
+
+		<!-- Link to all articles -->
+        <p class="articles-section-more">
+			<a href="<?= page('articles')->url() ?>" class="btn"><?php echo t('Show all articles') ?></a>
+		</p>
+      
+    </section>
+
+  </main>
+
+<?php snippet('footer') ?>
