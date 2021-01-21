3193aaee8863427db046333b49a09f73f7b5b31f
Johannes Isphording <mail@johannesisphording.de>
2021-01-21 17:21:17 +0100

    [php][template] copying template from projects page

--- site/templates/articles.php
+++ site/templates/articles.php
@@ -1,20 +1,30 @@
 <?php snippet('header') ?>
 
-	<main class="main" role="main">
+<!-- ARTICLES PAGE MAIN CONTENT -->
+<main class="main" role="main">
 
-		<!-- LOCAL PAGE HEADER -->
-		<header>
-			<!-- Intro -->
-			<div class="intro text <?= $page ?>">
-				<?= $page->text()->kirbytext() ?>
-			</div>
-		</header>
-		
-		<div>
-			<!-- Showcase Loop -->
-			<?php $site->displayShowcase('articles', 99) ?>
+	<!-- LOCAL PAGE HEADER -->
+	<header>
+		<!-- Intro -->
+		<div class="intro text <?= $page ?>">
+			<?= $page->text()->kirbytext() ?>
 		</div>
+	</header>
 
-	</main>
+	<!-- ARTICLE SHOWCASE -->
+	<section class="projects__showcase">
+
+		<!-- Showcase Loop -->
+		<?php $site->displayShowcase('articles', 99) ?>
+
+		<!-- Link to all articles -->
+		<p class="projects__showcase--more">
+			<a href="<?= page('articles')->url() ?>" class="btn"><?php echo t('Show all articles') ?></a>
+		</p>
+
+	</section>
+
+</main>
+<!-- END - ARTICLES PAGE MAIN CONTENT -->
 
 <?php snippet('footer') ?>
