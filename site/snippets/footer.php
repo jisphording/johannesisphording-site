c852576505c4ae4bf68d8dd68ccf3605a6a6a9d9
Johannes Isphording <mail@johannesisphording.de>
2021-01-21 12:52:30 +0100

    [php][css] clean up class names

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,34 +1,31 @@
 <!-- SITE HEAFOOTERDER -->
-<footer class="footer cf" role="contentinfo">
-    <div class="wrap wide">
+<footer role="contentinfo">
+	<!-- Footer -->
+	<section class="section__top">
+		
+		<!-- Logo / Branding -->
+		<div class="branding">
+			<a title="Logo links back to homepage" href="<?= $site->homePage()->url() ?>" rel="home">
+				<img src="<?= $site->uri() ?>/dist/img/logotype.svg" alt="Vector image of the branding logo">
+			</a>
+		</div>
 
-		<!-- Footer -->
-		<section class="section-top">
+		<!-- Footer Info -->
+		<?php
+		echo $site->copyright()->kirbytext();
+		echo $site->zusatz()->kirbytext();
+		echo $site->year()->kirbytext();
+		?> 
+	</section>
 
-			<!-- Logo / Branding -->
-			<div class="branding">
-				<a title="Logo links back to homepage" href="<?= $site->homePage()->url() ?>" rel="home">
-					<img src="<?= $site->uri() ?>/dist/img/logotype.svg" alt="Vector image of the branding logo">
-				</a>
-			</div>
-
-			<!-- Footer Info -->
-			<?php
-			echo $site->copyright()->kirbytext();
-			echo $site->zusatz()->kirbytext();
-			echo $site->year()->kirbytext();
-			?> 
-		</section>
-
-		<!-- Credits & Legal Information -->	
-		<section class="section-bottom">
-			<?php
-			echo $site->legal()->kirbytext();
-			echo $site->meta()->kirbytext();
-			?>   
-		</section>
-    </div>
-  </footer>
+	<!-- Credits & Legal Information -->	
+	<section class="section__bottom">
+		<?php
+		echo $site->legal()->kirbytext();
+		echo $site->meta()->kirbytext();
+		?>   
+	</section>
+</footer>
 
 <!-- ADDITIONAL LINKS -->
   
