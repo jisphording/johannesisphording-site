6220d3aa750ef1672d57420dc401f79fa2e785f9
Johannes Isphording <mail@johannesisphording.de>
2021-01-21 16:44:58 +0100

    [php] cleaner restructuring

--- site/templates/about.php
+++ site/templates/about.php
@@ -1,74 +1,69 @@
 <?php snippet('header') ?>
 
-	<main class="main about" role="main">
+<!-- ABOUT PAGE MAIN CONTENT -->
+<main class="main about" role="main">
 		
-		<!-- LOCAL PAGE HEADER -->
-		<header>
-			<section class="article intro">
-				<div class="keyvisual">
-				<img src="<?= $page->url() . '/' . $page->keyvisual() ?>">
-				<h1><?= $page->title() ?></h1>
-				</div>
-			</section>
-		</header>
-		
-		<!-- ABOUT CONTENT -->
-		<div class="text">
-		
-			<!-- About Headline  -->
-			<h3><?php echo t('About') ?></h3>
-			
-			<!-- Kirbytext - Intro -->
-			<article>
-				<?= $page->intro()->kirbytext() ?>
-			</article>
-			
-			<!-- about mood -->
-			<section class="mood full">
-				<img src="<?= $page->url() . '/' . $page->mood_01() ?>">
-				<p class="bildunterschrift"><?= $page->mood_01_txt() ?></p>
-			</section>
-			
-			<!-- about experience -->
-			<article class="experience">
-				<h3>Experience</h3>
-				<?= $page->experience()->kirbytext() ?>
-			</article>
-			
-			<!-- about mood images -->
-			<section class="moods">
-				<ul>
-					<li class="full border"><img src="<?= $page->url() . '/' . $page->mood_02() ?>"></li>
-					<li class="quarter"><img src="<?= $page->url() . '/' . $page->mood_03a() ?>"></li>
-					<li class="quarter"><img src="<?= $page->url() . '/' . $page->mood_03b() ?>"></li>
-				</ul>
-			</section>
-			
-			<!-- about awards & recognition -->
-			<article class="recognition">
-				<h3>Awards &amp; Recognition</h3>
-				<?= $page->recognition()->kirbytext() ?>
-			</article>
-		
-		</div>
-		<!-- END - ABOUT CONTENT -->
-		
-		<!-- OFFICE LOCATIONS -->
-		<article class="locations">
-			<h3>Locations</h3>
-
-			<?= $page->locations()->kirbytext() ?>
-
-			<?php snippet('visitenkarte-bln') ?>
-		</article>
-		
-		<!-- CONTACT INFORMATION -->
-		<section class="about-contact">
-			<div class="linklist">
-				<?= $page->text()->kirbytext() ?>
+	<!-- LOCAL PAGE HEADER -->
+	<header>
+		<section class="article intro">
+			<div class="keyvisual">
+			<img src="<?= $page->url() . '/' . $page->keyvisual() ?>">
+			<h1><?= $page->title() ?></h1>
 			</div>
 		</section>
+	</header>
 		
-  	</main>
+	<!-- ABOUT BODY CONTENT -->
+	<div class="text">
+		
+		<!-- About Headline  -->
+		<h3><?php echo t('About') ?></h3>
+			
+		<!-- Kirbytext - Intro -->
+		<article>
+			<?= $page->intro()->kirbytext() ?>
+		</article>
+			
+		<!-- about mood -->
+		<section class="mood full">
+			<img src="<?= $page->url() . '/' . $page->mood_01() ?>">
+			<p class="bildunterschrift"><?= $page->mood_01_txt() ?></p>
+		</section>
+			
+		<!-- about experience -->
+		<article class="experience">
+			<h3>Experience</h3>
+			<?= $page->experience()->kirbytext() ?>
+		</article>
+			
+		<!-- about mood images -->
+		<section class="moods">
+			<ul>
+				<li class="full border"><img src="<?= $page->url() . '/' . $page->mood_02() ?>"></li>
+				<li class="quarter"><img src="<?= $page->url() . '/' . $page->mood_03a() ?>"></li>
+				<li class="quarter"><img src="<?= $page->url() . '/' . $page->mood_03b() ?>"></li>
+			</ul>
+		</section>
+			
+		<!-- about awards & recognition -->
+		<article class="recognition">
+			<h3>Awards &amp; Recognition</h3>
+			<?= $page->recognition()->kirbytext() ?>
+		</article>
+		
+	</div>
+	<!-- END - ABOUT BODY CONTENT -->
+		
+	<!-- OFFICE LOCATIONS -->
+	<article class="locations">
+		<h3>Locations</h3>
+
+		<?= $page->locations()->kirbytext() ?>
+
+		<?php snippet('visitenkarte-bln') ?>
+	</article>
+		
+</main>
+<!-- END - ABOUT PAGE MAIN CONTENT -->
 
 <?php snippet('footer') ?>
